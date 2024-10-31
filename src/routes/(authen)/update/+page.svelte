87b2a2838f1d02ca0f1be7043053f671c959fe17
ChangerzaryX1602 <chatchanan.pa@kkumail.com>
2024-10-31 18:59:01 +0700

    Refactor authentication logic in +layout.svelte and update date format in +page.svelte

--- src/routes/(authen)/update/+page.svelte
+++ src/routes/(authen)/update/+page.svelte
@@ -77,7 +77,9 @@ import { goto } from '$app/navigation'
     <label>
         Birthdate:
         <input type="datetime" bind:value={birthdate} required />
+        <br>
         Format: YYYY-MM-DDTHH:MM:SS
+        If you forgot the time, please check the box below and Enter format YYYY-MM-DDT.
     </label>
     <label>
         <input type="checkbox" bind:checked={forgotTime} />
